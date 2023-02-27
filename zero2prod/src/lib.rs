99373af2c4a711e6ab377d806eec2596fea2cf8a
morteza <mrraeisi@outlook.com>
2023-02-27 21:16:14 +0330

    Impl basic POST/subscription handler  - Add serde@1 -F serde@1/derive crate

--- zero2prod/src/lib.rs
+++ zero2prod/src/lib.rs
@@ -1,15 +1,26 @@
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+use actix_web::{web, App, HttpServer, HttpResponse};
 use std::net::TcpListener;
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[derive(serde::Deserialize)]
+struct SubscriptionFormData {
+    name: String,
+    email: String,
+}
+
+async fn subscriptions(body: web::Form<SubscriptionFormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscriptions))
     })
         .listen(listener)?
         .run();
