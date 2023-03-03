1d9743235a8807735f36eee31f9df139ced3b5b0
morteza <mrraeisi@outlook.com>
2023-03-03 19:19:12 +0330

    Check for necessary commands to exist  - Create database using sqlx

--- zero2prod/scripts/init_db.sh
+++ zero2prod/scripts/init_db.sh
@@ -3,6 +3,19 @@
 set -x
 set -eo pipefail
 
+if ! [ -x "$(command -v psql)" ]; then
+  echo >&2 "Error: psql command not found"
+  exit 1
+fi
+
+if ! [ -x "$(command -v sqlx)" ]; then
+  echo >&2 "Error: sqlx not found"
+  echo >&2 "Use:"
+  echo >&2 "  cargo install --version=0.5.7 sqlx-cli --no-default-features --features postgres"
+  echo >&2 "to install it."
+  exit 1
+fi
+
 DB_USER=${POSTGRES_USER:=postgres}
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 DB_NAME="${POSTGRES_DB:=newsletter}"
@@ -16,3 +29,12 @@ docker run \
  -p "${DB_PORT}":5432 \
  -d postgres \
  postgres -N 1000
+
+export PGPASSWORD="$DB_PASSWORD"
+until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+  echo >&2 "Postgres is still unavailable - sleeping"
+  sleep 1
+done
+
+export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+sqlx database create
