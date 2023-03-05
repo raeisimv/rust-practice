62879d31837a6a729b808bb149100fc3ed3d1f9a
morteza <mrraeisi@outlook.com>
2023-03-05 21:28:35 +0330

    Add logging stuff  - Add `log` crate  - Add `env_logger` crate

--- zero2prod/src/main.rs
+++ zero2prod/src/main.rs
@@ -3,10 +3,13 @@ use sqlx::{PgPool};
 use tokio;
 use zero2prod;
 use zero2prod::conf;
+use env_logger::Env;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    println!("server -> initializing");
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    log::info!("initializing & reading conf");
+
     let conf = conf::get_configuration()
         .expect("failed to get_configuration");
 
@@ -14,10 +17,11 @@ async fn main() -> std::io::Result<()> {
     let db_pool = PgPool::connect(&conn_str)
         .await
         .expect("failed to connect to postgres");
-    println!("server -> connect to pg: {}", conf.database.database_name);
+    log::info!("connect to pg_pool: {}", conf.database.database_name);
 
     let address = format!("127.0.0.1:{}", conf.application_port);
-    println!("server -> starting at: {}", address);
+    log::info!("server is starting at: {}", address);
+
     let listener = TcpListener::bind(address)
         .expect("cannot find an TcpListener to bind");
 
--- zero2prod/src/routes/subscriptions.rs
+++ zero2prod/src/routes/subscriptions.rs
@@ -10,9 +10,12 @@ pub struct SubscriptionFormData {
 }
 
 pub async fn subscriptions(body: web::Form<SubscriptionFormData>, conn: web::Data<PgPool>) -> HttpResponse {
+    log::info!("Adding '{}', '{}' as a new subscriber", body.name, body.email);
+
     let at = Utc::now();
     let id = Uuid::new_v4();
 
+    log::info!("Saving subscriber details into the database");
     let res = sqlx::query!(r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
     VALUES ($1, $2, $3, $4)
@@ -23,9 +26,12 @@ pub async fn subscriptions(body: web::Form<SubscriptionFormData>, conn: web::Dat
         .await
         ;
     match res {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            log::info!("New Subscriber details have been saved");
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("/subscriptions: failed to execute query: {e}");
+            log::error!("/subscriptions: failed to execute query: {e:?}");
             HttpResponse::InternalServerError().finish()
         }
     }
