92f7e498124000bc5a48131645025fd8b43fe415
morteza <mrraeisi@outlook.com>
2023-03-09 16:37:29 +0330

    Refactor and break down the log functionality  - Use Procedural Macros to separate logging stuff

--- zero2prod/src/routes/subscriptions.rs
+++ zero2prod/src/routes/subscriptions.rs
@@ -2,7 +2,6 @@ use actix_web::{HttpResponse, web};
 use sqlx::{PgPool};
 use uuid::Uuid;
 use chrono::Utc;
-use tracing::Instrument;
 
 #[derive(serde::Deserialize)]
 pub struct SubscriptionFormData {
@@ -10,43 +9,37 @@ pub struct SubscriptionFormData {
     email: String,
 }
 
-pub async fn subscriptions(body: web::Form<SubscriptionFormData>, conn: web::Data<PgPool>) -> HttpResponse {
-    let request_id = Uuid::new_v4();
-    let request_span = tracing::info_span!(
-        "Adding a new subscriber",
-        %request_id,
-        subscriber_name = %body.name,
-        subscriber_email = %body.email
-    );
-    let _request_span = request_span.enter();
+#[tracing::instrument(
+name = "POST subscriptions"
+skip(body, conn)
+fields(
+request_id = % Uuid::new_v4(),
+subscriber_name = % body.name,
+subscriber_email = % body.email
+)
+)]
+pub async fn subscriptions(body: web::Form<SubscriptionFormData>, conn: web::Data<PgPool>)
+                           -> HttpResponse {
+    match db_insert_subscriber(&body, &conn).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
 
-    tracing::info!("request_id: {request_id} - Adding '{}', '{}' as a new subscriber", body.name, body.email);
-
+#[tracing::instrument(skip(body, conn))]
+async fn db_insert_subscriber(body: &SubscriptionFormData, conn: &PgPool)
+                              -> Result<(), sqlx::Error> {
     let at = Utc::now();
     let id = Uuid::new_v4();
-
-    tracing::info!("request_id: {request_id} - Saving subscriber details into the database");
-
-    let query_span = tracing::info_span!("Saving new subscriber details into the database");
-
-    let res = sqlx::query!(r#"
-    INSERT INTO subscriptions (id, email, name, subscribed_at)
-    VALUES ($1, $2, $3, $4)
-    "#,
+    sqlx::query!(
+        r#"INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)"#,
         id, body.email, body.name, at
     )
-        .execute(conn.get_ref())
-        .instrument(query_span)
+        .execute(conn)
         .await
-        ;
-    match res {
-        Ok(_) => {
-            tracing::info!("request_id: {request_id} - New Subscriber details have been saved");
-            HttpResponse::Ok().finish()
-        }
-        Err(e) => {
-            tracing::error!("request_id: {request_id} - /subscriptions: failed to execute query: {e:?}");
-            HttpResponse::InternalServerError().finish()
-        }
-    }
+        .map_err(|e| {
+            tracing::error!("failed to execute query: {e:?}");
+            e
+        })?;
+    Ok(())
 }
