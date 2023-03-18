1baa40d3ccce19799127ff4a4fde33533b2be61c
morteza <mrraeisi@outlook.com>
2023-03-18 21:27:22 +0330

    Add property-based testing  - Add `quickcheck`, `quickcheck_macros`, and `rand` dev deps

--- zero2prod/src/domain/subscriber_email.rs
+++ zero2prod/src/domain/subscriber_email.rs
@@ -22,9 +22,21 @@ impl AsRef<str> for SubscriberEmail {
 #[cfg(test)]
 mod tests {
     use crate::domain::SubscriberEmail;
-    use claim::{assert_ok, assert_err};
+    use claim::{assert_err};
     use fake::faker::internet::en::SafeEmail;
     use fake::Fake;
+    use rand::{rngs::StdRng, SeedableRng};
+
+    #[derive(Clone, Debug)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+            let mut rng = StdRng::seed_from_u64(u64::arbitrary(g));
+            let email = SafeEmail().fake_with_rng(&mut rng);
+            Self(email)
+        }
+    }
 
     #[test]
     fn empty_string_is_rejected() {
@@ -50,9 +62,8 @@ mod tests {
     //     assert_err!(SubscriberEmail::parse(email));
     // }
 
-    #[test]
-    fn valid_email_is_parsed_successfully() {
-        let email = SafeEmail().fake();
-        assert_ok!(SubscriberEmail::parse(email));
+    #[quickcheck_macros::quickcheck]
+    fn valid_email_is_parsed_successfully(email: ValidEmailFixture) -> bool {
+        SubscriberEmail::parse(email.0).is_ok()
     }
 }
