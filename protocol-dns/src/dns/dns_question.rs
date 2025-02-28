18c367d2dd8b0c33f62d8d0702c148306cc44215
Morteza Raeisi <mrraeisi@outlook.com>
2025-02-28 21:40:32 +0330

    impl DNS Question struct

---
+++ protocol-dns/src/dns/dns_question.rs
@@ -0,0 +1,23 @@
+use crate::dns::{BytePacketBuffer, QueryType, Result};
+
+#[derive(Clone, Debug)]
+pub struct DnsQuestion {
+    pub name: String,
+    pub qtype: QueryType,
+}
+
+impl DnsQuestion {
+    pub fn new(name: String, qtype: QueryType) -> Self {
+        Self { name, qtype }
+    }
+
+    pub fn read(&mut self, buf: &mut BytePacketBuffer) -> Result {
+        buf.read_qname(&mut self.name)?;
+        let qtype = buf.read_u16()?;
+        self.qtype = qtype.into();
+
+        let _class = buf.read_u16()?;
+
+        Ok(())
+    }
+}
--- protocol-dns/src/dns/mod.rs
+++ protocol-dns/src/dns/mod.rs
@@ -1,10 +1,12 @@
 mod dns_header;
+mod dns_question;
 mod errors;
 mod packet_buf;
 mod query_type;
 mod result_code;
 
 pub use dns_header::*;
+pub use dns_question::*;
 pub use errors::*;
 pub use packet_buf::*;
 pub use query_type::*;
