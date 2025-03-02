cdb4a6f4e2d40bc06a2ceee52c0020edfbc8c6a7
Morteza Raeisi <mrraeisi@outlook.com>
2025-03-02 21:15:45 +0330

    use new services and remove old way codes

--- protocol-dns/src/main.rs
+++ protocol-dns/src/main.rs
@@ -1,67 +1,15 @@
-use protocol_dns::dns::*;
-use std::io::Read;
+use protocol_dns::dns::Result;
+use protocol_dns::service::query_handler;
 
 fn main() -> Result {
-    let qname = "gmail.com";
-    let qtype = QueryType::MX;
-    let server = ("8.8.8.8", 53);
-    let socket = std::net::UdpSocket::bind(("0.0.0.0", 0))?;
-
-    let mut packet = DnsPacket::new();
-    packet.header.id = 6868;
-    packet.header.recursion_desired = true;
-    packet.questions.push(DnsQuestion::new(qname.into(), qtype));
-
-    let mut buf = BytePacketBuffer::new();
-    packet.write(&mut buf)?;
-    socket.send_to(&buf.buf[..buf.pos], server)?;
-
-    let mut buf = BytePacketBuffer::new();
-    socket.recv(&mut buf.buf)?;
-    let packet = DnsPacket::from_buffer(&mut buf)?;
-
-    println!("{:#?}", packet.header);
-
-    for q in packet.questions {
-        println!("{:#?}", q);
+    let socket = std::net::UdpSocket::bind(("0.0.0.0", 2053))?;
+    println!("DNS server is listening to: 0.0.0.0:2053");
+    loop {
+        match query_handler(&socket) {
+            Ok(_) => {}
+            Err(e) => {
+                eprintln!("error: {e:?}");
+            }
+        }
     }
-    for rec in packet.answers {
-        println!("{:#?}", rec);
-    }
-    for rec in packet.authorities {
-        println!("{:#?}", rec);
-    }
-    for rec in packet.resources {
-        println!("{:#?}", rec);
-    }
-
-    println!("app ran successfully.");
-    Ok(())
-}
-
-fn _main() -> Result {
-    let mut f = std::fs::File::open("data/query_packet.txt")?;
-    // let mut f = std::fs::File::open("data/query_resp.txt")?;
-    let mut buf = BytePacketBuffer::new();
-    f.read(&mut buf.buf)?;
-
-    let packet = DnsPacket::from_buffer(&mut buf)?;
-
-    println!("{:#?}", packet.header);
-
-    for q in packet.questions {
-        println!("{:#?}", q);
-    }
-    for rec in packet.answers {
-        println!("{:#?}", rec);
-    }
-    for rec in packet.authorities {
-        println!("{:#?}", rec);
-    }
-    for rec in packet.resources {
-        println!("{:#?}", rec);
-    }
-
-    println!("app ran successfully.");
-    Ok(())
 }
