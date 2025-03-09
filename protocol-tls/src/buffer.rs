b7fa34776d01596223b332e7af50e8463fa303ba
Morteza Raeisi <mrraeisi@outlook.com>
2025-03-09 23:33:54 +0330

    impl the Codec trait  - add DecodeError

--- protocol-tls/src/buffer.rs
+++ protocol-tls/src/buffer.rs
@@ -1,3 +1,6 @@
+use crate::{DecodeError, IntoU8, TlsResult};
+use std::fmt::Debug;
+
 pub struct BufReader<'a> {
     buf: &'a [u8],
     pos: usize,
@@ -22,5 +25,39 @@ impl<'a> BufReader<'a> {
         self.pos += size;
 
         Some(taken)
+    }
+}
+
+pub trait Codec: Debug + Sized {
+    fn encode(&self, buf: &mut Vec<u8>);
+    fn decode(&self, buf: &mut BufReader<'_>) -> TlsResult<Self, DecodeError>;
+}
+
+impl Codec for u8 {
+    fn encode(&self, buf: &mut Vec<u8>) {
+        buf.push(*self);
+    }
+    fn decode(&self, buf: &mut BufReader<'_>) -> TlsResult<Self, DecodeError> {
+        match buf.take(1) {
+            Some(x) => Ok(x[0]),
+            _ => Err(DecodeError::InvalidMessage("missing u8".into())),
+        }
+    }
+}
+
+impl Codec for u16 {
+    fn encode(&self, buf: &mut Vec<u8>) {
+        buf.push(self.byte_at(1));
+        buf.push(self.byte_at(0));
+    }
+
+    fn decode(&self, buf: &mut BufReader<'_>) -> TlsResult<Self, DecodeError> {
+        match buf.take(2) {
+            None => Err(DecodeError::InvalidMessage("missing u16".into())),
+            Some(x) => {
+                let x = ((x[0] as u16) << 8) & (x[1] as u16);
+                Ok(x)
+            }
+        }
     }
 }
--- protocol-tls/src/errors.rs
+++ protocol-tls/src/errors.rs
@@ -3,3 +3,8 @@
 pub type TlsError = Box<dyn std::error::Error>;
 
 pub type TlsResult<T = (), E = TlsError> = Result<T, E>;
+
+#[derive(Clone, Debug)]
+pub enum DecodeError {
+    InvalidMessage(String),
+}
