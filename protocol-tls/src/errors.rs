b219b82e23abc65724d1ed3090adf285e772f39b
Morteza Raeisi <mrraeisi@outlook.com>
2025-03-30 22:07:33 +0330

    impl Display for the error enum

--- protocol-tls/src/errors.rs
+++ protocol-tls/src/errors.rs
@@ -1,5 +1,7 @@
 // Project ergonomic error and result types
 
+use std::fmt::{Display, Formatter};
+
 pub type TlsError = Box<dyn std::error::Error>;
 
 pub type TlsResult<T = (), E = TlsError> = Result<T, E>;
@@ -9,4 +11,15 @@ pub enum DecodeError {
     InvalidMessage(String),
     InvalidConversion(String),
     CryptoError(String),
+}
+
+impl Display for DecodeError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let txt = match self {
+            DecodeError::InvalidMessage(x) => format!("Invalid Message: {x}"),
+            DecodeError::InvalidConversion(x) => format!("Invalid Conversion: {x}"),
+            DecodeError::CryptoError(x) => format!("Crypto Error: {x}"),
+        };
+        write!(f, "{}", txt)
+    }
 }
