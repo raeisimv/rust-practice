097ea1d27ca721be9e409252064be58f51add976
Morteza Raeisi <mrraeisi@outlook.com>
2025-04-12 21:31:21 +0330

    impl the datatype parser  - extend the list of supported types

--- db-sql/src/parser/create.rs
+++ db-sql/src/parser/create.rs
@@ -1,13 +1,32 @@
-use crate::parser::{identifier, SqlStatement};
+use crate::parser::{SqlDataType, SqlStatement, identifier};
 use nom::{
-    branch::alt, bytes::tag_no_case,
+    IResult, Parser,
+    branch::alt,
+    bytes::tag_no_case,
     character::complete::{space0, space1},
     combinator::map,
     sequence::preceded,
-    IResult,
-    Parser,
 };
 
+fn data_type(input: &str) -> IResult<&str, SqlDataType> {
+    let (input, dtyp) = alt((
+        tag_no_case("INT"),
+        tag_no_case("STRING"),
+        tag_no_case("TEXT"),
+        tag_no_case("BIT"),
+        tag_no_case("UUID"),
+        tag_no_case("FLOAT"),
+    ))
+    .parse(input)?;
+
+    let Ok(dtyp) = SqlDataType::try_from(dtyp) else {
+        return Err(nom::Err::Failure(nom::error::make_error(
+            input,
+            nom::error::ErrorKind::Alpha,
+        )));
+    };
+    Ok((input, dtyp))
+}
 fn constraint(input: &str) -> IResult<&str, String> {
     let (input, constraint) = alt((
         tag_no_case("DEFAULT"),
--- db-sql/src/parser/mod.rs
+++ db-sql/src/parser/mod.rs
@@ -20,11 +20,32 @@ pub enum SqlStatement {
 pub enum SqlDataType {
     Integer,
     String,
+    Boolean,
+    Uuid,
+    Text,
+    Float,
 }
+impl TryFrom<&str> for SqlDataType {
+    type Error = nom::Err<&'static str>;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        Ok(match value.to_uppercase().as_str() {
+            "INT" => SqlDataType::Integer,
+            "STRING" => SqlDataType::String,
+            "UUID" => SqlDataType::Uuid,
+            "TEXT" => SqlDataType::Text,
+            "BIT" => SqlDataType::Boolean,
+            "FLOAT" => SqlDataType::Float,
+            _ => return Err(Self::Error::Failure("Invalid DataType")),
+        })
+    }
+}
+
 #[derive(Clone, Debug)]
 pub struct ColumnDefinition {
     pub name: String,
     pub data_type: SqlDataType,
+    pub constraint: Option<String>,
 }
 
 fn identifier(input: &str) -> IResult<&str, String> {
