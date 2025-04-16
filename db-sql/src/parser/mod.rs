ed410155727a4c1f713e8598f7d79a353cbca672
Morteza Raeisi <mrraeisi@outlook.com>
2025-04-16 22:10:13 +0330

    impl SqlValue type  - add parser for the types

--- db-sql/src/parser/mod.rs
+++ db-sql/src/parser/mod.rs
@@ -5,7 +5,16 @@ mod select;
 pub use create::*;
 pub use select::*;
 
-use nom::{IResult, Parser, bytes::complete::take_while1, combinator::map};
+use nom::{
+    character::char,
+    bytes::streaming::tag_no_case,
+    branch::alt,
+    sequence::delimited,
+    bytes::complete::take_while1,
+    combinator::map,
+    IResult,
+    Parser
+};
 
 pub fn parse_sql(input: &str) -> IResult<&str, SqlStatement> {
     if let Ok(x) = parse_select_query(input.trim()) {
@@ -20,7 +29,7 @@ pub fn parse_sql(input: &str) -> IResult<&str, SqlStatement> {
     }
 }
 
-#[derive(Clone, Debug, PartialEq, Eq)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum SqlStatement {
     Select {
         table: String,
@@ -33,7 +42,7 @@ pub enum SqlStatement {
     },
     Insert {
         table: String,
-        values: Vec<String>,
+        values: Vec<SqlValue>,
     },
 }
 
@@ -62,6 +71,14 @@ impl TryFrom<&str> for SqlDataType {
     }
 }
 
+#[derive(Clone, Debug, PartialEq)]
+pub enum SqlValue {
+    String(String),
+    Boolean(bool),
+    Integer(i32),
+    Float(f64),
+}
+
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct ColumnDefinition {
     pub name: String,
@@ -72,4 +89,30 @@ pub struct ColumnDefinition {
 fn identifier(input: &str) -> IResult<&str, String> {
     let x = take_while1(|c: char| c.is_alphanumeric() || c == '_');
     map(x, |s: &str| s.to_string()).parse(input)
+}
+
+fn string_value(input: &str) -> IResult<&str, String> {
+    delimited(
+        char('\''),
+        map(take_while1(|x| x != '\''), |s: &str| s.to_string()),
+        char('\''),
+    )
+    .parse(input)
+}
+
+fn boolean_value(input: &str) -> IResult<&str, bool> {
+    let (input, val) = alt((tag_no_case("true"), tag_no_case("false"))).parse(input)?;
+
+    if val.to_lowercase() == "true" {
+        Ok((input, true))
+    } else {
+        Ok((input, false))
+    }
+}
+
+fn int_value(input: &str) -> IResult<&str, i32> {
+    map(take_while1(|x: char| x.is_numeric()), |s: &str| {
+        s.parse::<i32>().unwrap_or_default()
+    })
+    .parse(input)
 }
