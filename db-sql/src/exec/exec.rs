528978c8d8da57401e878d855e56dd00af6c69b9
Morteza Raeisi <mrraeisi@outlook.com>
2025-04-29 19:14:08 +0330

    move the execution context into its own file

---
+++ db-sql/src/exec/exec.rs
@@ -0,0 +1,38 @@
+use crate::errors::{DbResult, ExecutionError};
+use crate::exec::Table;
+use crate::parser::{Identifier, SqlStatement};
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct ExecutionContext {
+    tables: HashMap<Identifier, Table>,
+}
+
+impl ExecutionContext {
+    pub fn new() -> Self {
+        Self {
+            tables: HashMap::new(),
+        }
+    }
+    pub fn exec(&mut self, cmd: &SqlStatement) -> DbResult<(), ExecutionError> {
+        match cmd {
+            SqlStatement::Select { table, .. } => {
+                let Some(tbl) = self.tables.get(table) else {
+                    return Err(ExecutionError::TableNotFound);
+                };
+            }
+            SqlStatement::Insert { table, values } => {
+                let Some(tbl) = self.tables.get(table) else {
+                    return Err(ExecutionError::TableNotFound);
+                };
+            }
+            SqlStatement::Create { table, .. } => {
+                if self.tables.get(table).is_some() {
+                    return Err(ExecutionError::TableAlreadyExists);
+                };
+            }
+            _ => {}
+        }
+        Ok(())
+    }
+}
--- db-sql/src/exec/mod.rs
+++ db-sql/src/exec/mod.rs
@@ -1,41 +1,4 @@
+mod exec;
 mod table;
 
-use crate::{
-    errors::{DbResult, ExecutionError},
-    parser::{Identifier, SqlStatement},
-};
-use std::collections::HashMap;
 pub use table::*;
-
-#[derive(Debug)]
-pub struct ExecutionContext {
-    tables: HashMap<Identifier, Table>,
-}
-impl ExecutionContext {
-    pub fn new() -> Self {
-        Self {
-            tables: HashMap::new(),
-        }
-    }
-    pub fn exec(&mut self, cmd: &SqlStatement) -> DbResult<(), ExecutionError> {
-        match cmd {
-            SqlStatement::Select { table, .. } => {
-                let Some(tbl) = self.tables.get(table) else {
-                    return Err(ExecutionError::TableNotFound);
-                };
-            }
-            SqlStatement::Insert { table, values } => {
-                let Some(tbl) = self.tables.get(table) else {
-                    return Err(ExecutionError::TableNotFound);
-                };
-            }
-            SqlStatement::Create { table, .. } => {
-                if self.tables.get(table).is_some() {
-                    return Err(ExecutionError::TableAlreadyExists);
-                };
-            }
-            _ => {}
-        }
-        Ok(())
-    }
-}
